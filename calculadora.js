9deaeed6b78f98f9718397390a654e76a26f1a0b
Euler-Tosati <eulertosati777@gmail.com>
2023-02-08 04:59:52 -0300

    arg não estava definida, nome mudado para argsna linha 40

---
+++ calculadora.js
@@ -0,0 +1,42 @@
+const soma = () => {
+    console.log(parseInt(args[0]) + parseInt(args[1]));
+};
+const sub = () => {
+    console.log(parseInt(args[0]) - parseInt(args[1]));  
+}
+
+const args = process.argv.slice(2);
+
+switch (args[0]) {
+    case 'soma':
+        soma();
+    break;
+
+    case 'sub':
+        sub();
+    break;
+
+    default:
+        console.log('does not support', args[0]);
+const soma = () => {
+    console.log(parseInt(args[0]) + parseInt(args[1]));
+};
+const sub = () => {
+    console.log(parseInt(args[0]) - parseInt(args[1]));  
+}
+
+const args = process.argv.slice(2);
+
+switch (args[0]) {
+    case 'soma':
+        soma();
+    break;
+
+    case 'sub':
+        sub();
+    break;
+
+    default:
+        console.log('does not support', args[0]);
+}
+}
