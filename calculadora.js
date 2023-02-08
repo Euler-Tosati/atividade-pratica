b4774cd74df6d3d320d8f30ae31d6b7c7bf4f46e
Euler-Tosati <eulertosati777@gmail.com>
2023-02-08 05:29:34 -0300

    refactor: calculator abstraction

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,12 @@
+var x = args[0];
+var y = args[2];
+var operator = args[1];
+
+function evaluate(param1, param2, operator) {
+  return eval(param1 + operator + param2);
+}
+
+if ( console.log( evaluate(x, y, operator) ) ) {}
 
 const soma = () => {
     console.log(parseInt(args[0]) + parseInt(args[1]));
